fa18a004bba97fd3041c76681fc64046ce21d07e
Timur Saidov <timur.saidov@devexperts.com>
2023-01-27 16:05:07 +0700

    update jmeter config

--- jmeter/run.sh
+++ jmeter/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-JVM_ARGS="-Xms1024m -Xmx1024m" jmeter -n -t SibCoins.jmx
+JVM_ARGS="-Xms1024m -Xmx1024m" jmeter \
+        -JNUMBER_OF_THREADS=10 \
+        -JRAMP_UP_PERIOD_SECONDS=10 \
+        -JLOOP_COUNT=2 \
+        -n -t SibCoins.jmx
