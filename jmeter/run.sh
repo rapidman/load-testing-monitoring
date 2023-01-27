83b944fdd1e0581555a6461220f948bcfedbb493
Timur Saidov <timur.saidov@devexperts.com>
2023-01-27 16:10:52 +0700

    update jmeter config

--- jmeter/run.sh
+++ jmeter/run.sh
@@ -4,4 +4,7 @@ JVM_ARGS="-Xms1024m -Xmx1024m" jmeter \
         -JNUMBER_OF_THREADS=10 \
         -JRAMP_UP_PERIOD_SECONDS=10 \
         -JLOOP_COUNT=2 \
+        -JBASE_HOST=sibedgecoinstest.development.com \
+        -JPORT_NUMBER=5001 \
+        -JPROTOCOL=http \
         -n -t SibCoins.jmx
